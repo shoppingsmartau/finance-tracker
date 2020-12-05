e7103fb2e7b63d9c21df4b9372ffbcd5b7e6151e
Shopping SMart Au <shopping_smart_au@outlook.com>
2020-12-05 01:24:12 +0000

    section 248

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  root 'pages#home'
-  get 'about', to: 'welcome#about'
+  #root 'pages#home'
+  root 'welcome#index'
+  get 'about', to: 'pages#about'
   
 end
 
